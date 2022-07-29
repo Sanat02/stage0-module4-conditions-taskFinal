781e2cde3c098c87f82fbbf0558ee192abe6d5ca
Sanat02 <86793142+Sanat02@users.noreply.github.com>
2022-07-29 11:42:12 +0600

    Update TriangleSidesValidator.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -4,11 +4,11 @@ public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
         if(firstSide<=secondSide+thirdSide||secondSide<=firstSide+thirdSide||thirdSide<=firstSide+secondSide)
         {
-            System.out.println("this is a valid triangle");
+            logger.log("this is a valid triangle");
         }
         else
         {
-            System.out.println("it's not a triangle");
+            logger.log("it's not a triangle");
         }
 
     }
