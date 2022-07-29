14f58fff2db3a41481414a44311e3f18634e9c07
Sanat02 <86793142+Sanat02@users.noreply.github.com>
2022-07-29 11:55:05 +0600

    Update DaysInMonth.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,28 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if(month==9||month==4||month==6||month==11)
+      {
+          System.out.println("30");
+      }
+      else if(month==2&&year%4==0)
+      {
+          System.out.println("29");
+      }
+      else if(month==2&&year%4!=0)
+      {
+          System.out.println("28");
+      }
+      else if(month>12||month<=0)
+      {
+             System.out.println("invalid date");
+
+      }
+      else
+      {
+          System.out.println("31");
+      }
+
 
     }
 }
